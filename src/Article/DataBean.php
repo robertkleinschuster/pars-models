8da65cc8f1de5707f8b784516324834bf77f1d7b
robertkleinschuster <robert.kleinschuster@niceshops.com>
2021-01-18 19:40:30 +0100

    cmspost displayname in frontend

--- src/Article/DataBean.php
+++ src/Article/DataBean.php
@@ -10,4 +10,6 @@ class DataBean extends AbstractBaseBean
 {
     public ?bool $vote_once = false;
     public ?string $contact_email = '';
+    public ?string $User_Displayname_Create = null;
+    public ?string $User_Displayname_Edit = null;
 }
