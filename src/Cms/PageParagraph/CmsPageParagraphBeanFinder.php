adfa5d2e66f70f6f0a1582e1326119a6334d16ce
robertkleinschuster <robert.kleinschuster@niceshops.com>
2021-02-13 13:09:43 +0100

    DatabaseLoader definitions as objects

--- src/Cms/PageParagraph/CmsPageParagraphBeanFinder.php
+++ src/Cms/PageParagraph/CmsPageParagraphBeanFinder.php
@@ -18,29 +18,93 @@ class CmsPageParagraphBeanFinder extends AbstractBeanFinder
         $loader->addColumn('CmsParagraph_ID', 'CmsParagraph_ID', 'CmsPage_CmsParagraph', 'CmsParagraph_ID', true);
         $loader->addColumn('CmsParagraphState_Code', 'CmsParagraphState_Code', 'CmsParagraph', 'CmsParagraph_ID');
         $loader->addColumn('CmsParagraphType_Code', 'CmsParagraphType_Code', 'CmsParagraph', 'CmsParagraph_ID');
-        $loader->addColumn('CmsParagraphType_Template', 'CmsParagraphType_Template', 'CmsParagraphType', 'CmsParagraphType_Code', false, 'CmsParagraphType_Code', [], 'CmsParagraph');
-        $loader->addColumn('Person_ID_Create', 'Person_ID_Create', 'CmsPage_CmsParagraph', 'CmsParagraph_ID');
-        $loader->addColumn('Person_ID_Edit', 'Person_ID_Edit', 'CmsPage_CmsParagraph', 'CmsParagraph_ID');
-        $loader->addColumn('Timestamp_Create', 'Timestamp_Create', 'CmsPage_CmsParagraph', 'CmsParagraph_ID');
-        $loader->addColumn('Timestamp_Edit', 'Timestamp_Edit', 'CmsPage_CmsParagraph', 'CmsParagraph_ID');
-        $loader->addColumn('CmsPage_CmsParagraph_Order', 'CmsPage_CmsParagraph_Order', 'CmsPage_CmsParagraph', 'CmsParagraph_ID');
-        $loader->addColumn('Article_ID', 'Article_ID', 'CmsParagraph', 'CmsParagraph_ID', false, null, ['Article']);
-        $loader->addColumn('Article_Code', 'Article_Code', 'Article', 'Article_ID', false, null, [], 'CmsParagraph');
-        $loader->addColumn('Article_Data', 'Article_Data', 'Article', 'Article_ID', false, null, [], 'CmsParagraph');
-        $loader->addColumn('ArticleTranslation_Name', 'ArticleTranslation_Name', 'ArticleTranslation', 'Article_ID', false, null, [], 'Article');
-        $loader->addColumn('ArticleTranslation_Code', 'ArticleTranslation_Code', 'ArticleTranslation', 'Article_ID', false, null, [], 'Article');
-        $loader->addColumn('ArticleTranslation_Host', 'ArticleTranslation_Host', 'ArticleTranslation', 'Article_ID', false, null, [], 'Article');
-        $loader->addColumn('ArticleTranslation_Active', 'ArticleTranslation_Active', 'ArticleTranslation', 'Article_ID', false, null, [], 'Article');
-        $loader->addColumn('ArticleTranslation_Title', 'ArticleTranslation_Title', 'ArticleTranslation', 'Article_ID', false, null, [], 'Article');
-        $loader->addColumn('ArticleTranslation_Keywords', 'ArticleTranslation_Keywords', 'ArticleTranslation', 'Article_ID', false, null, [], 'Article');
-        $loader->addColumn('ArticleTranslation_Heading', 'ArticleTranslation_Heading', 'ArticleTranslation', 'Article_ID', false, null, [], 'Article');
-        $loader->addColumn('ArticleTranslation_SubHeading', 'ArticleTranslation_SubHeading', 'ArticleTranslation', 'Article_ID', false, null, [], 'Article');
-        $loader->addColumn('ArticleTranslation_Path', 'ArticleTranslation_Path', 'ArticleTranslation', 'Article_ID', false, null, [], 'Article');
-        $loader->addColumn('ArticleTranslation_Teaser', 'ArticleTranslation_Teaser', 'ArticleTranslation', 'Article_ID', false, null, [], 'Article');
-        $loader->addColumn('ArticleTranslation_Text', 'ArticleTranslation_Text', 'ArticleTranslation', 'Article_ID', false, null, [], 'Article');
-        $loader->addColumn('ArticleTranslation_Footer', 'ArticleTranslation_Footer', 'ArticleTranslation', 'Article_ID', false, null, [], 'Article');
-        $loader->addColumn('Locale_Code', 'Locale_Code', 'ArticleTranslation', 'Article_ID', false, null, [], 'Article');
-        $loader->addColumn('File_ID', 'File_ID', 'ArticleTranslation', 'Article_ID', false, null, [], 'Article');
+        $loader->addColumn('CmsParagraphType_Template')
+            ->setTable('CmsParagraphType')
+            ->setJoinField('CmsParagraphType_Code')
+            ->setJoinTableSelf('CmsParagraph');
+        $loader->addColumn('Person_ID_Create')
+            ->setTable('CmsPage_CmsParagraph')
+            ->setJoinField('CmsParagraph_ID');
+        $loader->addColumn('Person_ID_Edit')
+            ->setTable('CmsPage_CmsParagraph')
+            ->setJoinField('CmsParagraph_ID');
+        $loader->addColumn('Timestamp_Create')
+            ->setTable('CmsPage_CmsParagraph')
+            ->setJoinField('CmsParagraph_ID');
+        $loader->addColumn('Timestamp_Edit')
+            ->setTable('CmsPage_CmsParagraph')
+            ->setJoinField('CmsParagraph_ID');
+        $loader->addColumn('CmsPage_CmsParagraph_Order')
+            ->setTable('CmsPage_CmsParagraph')
+            ->setJoinField('CmsParagraph_ID');
+        $loader->addColumn('Article_ID')
+            ->setTable('CmsParagraph')
+            ->setJoinField('CmsParagraph_ID')
+            ->setAdditionalTableList(['Article']);
+        $loader->addColumn('Article_Code')
+            ->setTable('Article')
+            ->setJoinField('Article_ID')
+            ->setJoinTableSelf('CmsParagraph');
+        $loader->addColumn('Article_Data')
+            ->setTable('Article')
+            ->setJoinField('Article_ID')
+            ->setJoinTableSelf('CmsParagraph');
+        $loader->addColumn('ArticleTranslation_Name')
+            ->setTable('ArticleTranslation')
+            ->setJoinField('Article_ID')
+            ->setJoinTableSelf('Article');
+        $loader->addColumn('ArticleTranslation_Code')
+            ->setTable('ArticleTranslation')
+            ->setJoinField('Article_ID')
+            ->setJoinTableSelf('Article');
+        $loader->addColumn('ArticleTranslation_Host')
+            ->setTable('ArticleTranslation')
+            ->setJoinField('Article_ID')
+            ->setJoinTableSelf('Article');
+        $loader->addColumn('ArticleTranslation_Active')
+            ->setTable('ArticleTranslation')
+            ->setJoinField('Article_ID')
+            ->setJoinTableSelf('Article');
+        $loader->addColumn('ArticleTranslation_Title')
+            ->setTable('ArticleTranslation')
+            ->setJoinField('Article_ID')
+            ->setJoinTableSelf('Article');
+        $loader->addColumn('ArticleTranslation_Keywords')
+            ->setTable('ArticleTranslation')
+            ->setJoinField('Article_ID')
+            ->setJoinTableSelf('Article');
+        $loader->addColumn('ArticleTranslation_Heading')
+            ->setTable('ArticleTranslation')
+            ->setJoinField('Article_ID')
+            ->setJoinTableSelf('Article');
+        $loader->addColumn('ArticleTranslation_SubHeading')
+            ->setTable('ArticleTranslation')
+            ->setJoinField('Article_ID')
+            ->setJoinTableSelf('Article');
+        $loader->addColumn('ArticleTranslation_Path')
+            ->setTable('ArticleTranslation')
+            ->setJoinField('Article_ID')
+            ->setJoinTableSelf('Article');
+        $loader->addColumn('ArticleTranslation_Teaser')
+            ->setTable('ArticleTranslation')
+            ->setJoinField('Article_ID')
+            ->setJoinTableSelf('Article');
+        $loader->addColumn('ArticleTranslation_Text')
+            ->setTable('ArticleTranslation')
+            ->setJoinField('Article_ID')
+            ->setJoinTableSelf('Article');
+        $loader->addColumn('ArticleTranslation_Footer')
+            ->setTable('ArticleTranslation')
+            ->setJoinField('Article_ID')
+            ->setJoinTableSelf('Article');
+        $loader->addColumn('Locale_Code')
+            ->setTable('ArticleTranslation')
+            ->setJoinField('Article_ID')
+            ->setJoinTableSelf('Article');
+        $loader->addColumn('File_ID')
+            ->setTable('ArticleTranslation')
+            ->setJoinField('Article_ID')
+            ->setJoinTableSelf('Article');
         $loader->addOrder('CmsPage_CmsParagraph_Order');
         $this->addLinkedFinder(new FileBeanFinder($adapter), 'File_BeanList', 'File_ID', 'File_ID');
         parent::__construct($loader, new CmsPageParagraphBeanFactory());
